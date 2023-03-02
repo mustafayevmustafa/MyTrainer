3b515010f4d549e591c2492b57bb201d070866a6
mustafa99 <mustafayevmustafa99@gmail.com>
2023-03-02 14:15:52 +0400

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+<<<<<<< HEAD
     return view('welcome');
+=======
+    return view('index');
+>>>>>>> 3637579 (first commit)
 });
