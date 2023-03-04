3e3142aed61ca18ed7544a91cd920f8db675ec3b
mustafa99 <mustafayevmustafa99@gmail.com>
2023-03-04 15:51:00 +0400

    frontend

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-<<<<<<< HEAD
-    return view('welcome');
-=======
     return view('index');
->>>>>>> 3637579 (first commit)
 });
